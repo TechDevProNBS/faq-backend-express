e81cc469c974cb419126c1aa8366552c3f588d7f
lewis <lewis.sayle@sky.com>
2019-12-03 16:48:45 +0000

    get, put and delete requests done in AnswerRoute, post is unfinished

--- Express.js
+++ Express.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var questions = require('./QuestionRoute');
+var answers = require('./AnswerRoute');
 var express = require("express");
 var bodyparser = require("body-parser");
 var app = express();
@@ -8,6 +9,6 @@ app.use(bodyparser.urlencoded({ extended: true }));
 // ...
 
 app.use('/Questions', questions);
-
+app.use('/Answers',answers);
 
 app.listen(4001);
