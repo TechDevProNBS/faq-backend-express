69f1896b40bc1bf703565f46a42fb92e6f72f6cd
Oli <Olirecord@hotmail.co.uk>
2019-12-04 10:22:27 +0000

    bug fixes in answer requests and 2 comment requests added

--- AnswerRoute.js
+++ AnswerRoute.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var cors = require("cors");
 var con = mysqlQ();
 var app = express();
-//app.use(session({ secret: 'Secretses Hobbitses' }));
+app.use(session({ secret: 'Secretses Hobbitses' }));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
@@ -21,50 +21,48 @@ router.use(function (req, res, next) {
 router.get('/RecentA', function (req, res) {
     let q_id = req.body.q_id
     con.connect(function (err) {
-        con.query(`select * from answers LEFT JOIN answerrating ar ON answers.a_id = ar.a_id WHERE answers.q_id = "${q_id}" ORDER by ar.rating desc`, function (err, results) {
+        con.query(`select * from answers a LEFT JOIN answerrating ar ON a.a_id = ar.a_id WHERE a.q_id = ${q_id} ORDER by ar.rating desc`, function (err, results) {
             res.send(results)
         })
     })
 })
 
-//Delete requests for answers
+//Delete requests for answers DELETING FROM RATING TABLE FIRST
 router.delete('/DelA', function (req, res) {
     let a_id = req.body.a_id
     con.connect(function (err) {
+        con.query(`delete from answerrating where a_id = ${a_id}`, function (err, results) {
+        })
         con.query(`delete from answers where a_id = ${a_id}`, function (err, results) {
-            res.send({ response: "answer deleted" })
+            res.send({ response: "answer deletedw" })
         })
     })
 })
 
 //put request for answers
-router.put('/UpdateA',function(req,res){
+router.put('/UpdateA', function (req, res) {
     let updA = req.body.updA
     let a_id = req.body.a_id
-    let q_id = req.body.q_id
-    con.connect(function(err){
-        con.query(`Update answers SET answer = "${updA}" WHERE a_id = ${a_id} AND q_id = ${q_id}`,function(err,results){
+    con.connect(function (err) {
+        con.query(`Update answers SET answer = "${updA}" WHERE a_id = ${a_id}`, function (err, results) {
             res.send(results)
         })
     })
 })
 
-//post request for answers (still needs work)
-
-router.post('/PostA',function(req,res){
+//POST REQUEST FOR ANSWERS
+router.post('/PostA', function (req, res) {
     let answer = req.body.answer
     let q_id = req.body.q_id
     let u_id = req.body.u_id
-    con.connect(function (err){
-        con.query(`insert into answers values(0,'${answer}','${u_id}','${q_id}',CURRENT_TIMESTAMP())`),function(err,results){
-            if(err) {console.log(err)}
+    con.connect(function (err) {
+        con.query(`insert into answers values(0,'${answer}','${u_id}','${q_id}',CURRENT_TIMESTAMP())`, function (err, results) {
+            if (err) { console.log(err) }
             res.send(results)
         })
     })
 })
 
 
-
-router.get('')
 
 module.exports = router
